3bbf8ac5e078a66b954b3812906a58ff7280001b
Jerry Xie <jerry@v1020-wn-201-130.campus-dynamic.uwaterloo.ca>
2018-05-26 02:30:38 -0400

    Added helper js functions.

--- api/functions/main/__main__.js
+++ api/functions/main/__main__.js
@@ -1,12 +1,13 @@
+const fb = require('../../src/facebook.js')
+
 /**
 * Main friend finding function
 * @param {string} room 
 * @returns {string}
 */
 module.exports = (room = 'world', context, callback) => {
-
-    callback(null, `hello ${room}`);
+    let result = fb(20);
+    callback(null, `hello ${result}`);
   
   };
-  
   
