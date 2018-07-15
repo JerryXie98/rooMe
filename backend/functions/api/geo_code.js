bfbbe15892b67f6ee9064748b63bc481da720969
Jerry Xie <jerry_xie@hotmail.com>
2018-07-15 15:37:53 -0400

    Cleaned up geo_code function

--- backend/functions/api/geo_code.js
+++ backend/functions/api/geo_code.js
@@ -1,28 +1,38 @@
+function averageCoord(northEast, southWest) {
+    return {
+        lat: (northEast.lat + southWest.lat) / 2.0,
+        lng: (northEast.lng + southWest.lng) / 2.0
+    }
+} 
 
 /**
  * Make requests to convert address to latlng
- * @param {string} addy
- * @param {string} api_key
+ * @param {string} address
  * @returns {any}
  */
 
-module.exports = async (addy, api_key, context) => {
+module.exports = async (address,  context) => {
 
     var googleMapsClient = require('@google/maps').createClient({
-        key: api_key,
+        key: 'AIzaSyAM0PNq8xe3q6oml9Yj-IpQaV0_yzYnKHA',
         Promise: Promise
     });
 
-	await googleMapsClient.geocode({address: addy})
+	await googleMapsClient.geocode({address: address})
      .asPromise()
      .then((res) => {
-         //console.log(res.json.results);
-         data = res.json.results;
-
+         if (res.json.results.length !== 1) {
+             return {
+                 "error":  "Error parsing address to geo-code"
+             }
+         } else {
+             const bounds = res.json.results[0].geometry.bounds
+             data = averageCoord(bounds.northeast, bounds.southwest)
+         }
+         return data
      })
      .catch((err) => {
          console.log(err);
          return {"error" : err}
      })
-    return data;
 }
